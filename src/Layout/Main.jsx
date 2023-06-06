f59d9ace139a23b16b6bd547397befbc099c6ff4
Sihab Hossain <sihabhossain380@gmail.com>
2023-06-06 22:42:35 +0600

    first commit

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Pages/Navbar/Navbar";
+import Footer from "../Pages/Footer/Footer";
+import Home from "../Pages/Home/Home";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Home></Home>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Main;
---
+++ src/Pages/Login/Resgistration.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Resgistration = () => {
+  return (
+    <div>
+      <h2>THis is Registration page</h2>
+    </div>
+  );
+};
+
+export default Resgistration;
